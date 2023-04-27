05eb542fa76c23de7b9faf61eab21c5c4ebaeb24
yuvalyv <Yuvalyv@gmail.com>
2023-04-27 11:57:19 +0300

    add shirs node files

---
+++ src/Constants.js
@@ -0,0 +1,82 @@
+// Page Names
+
+export const SIGN_IN = 'sign_in';
+export const SIGN_UP = 'sign_up';
+export const REG_FORM = 'registration_form';
+export const PROCESS_COMPLETION_FORM = 'process-completion';
+export const HOME_PAGE = 'home-page';
+export const LANDING_PAGE = 'landing-page';
+export const CHOOSE_ROLE_PAGE = 'choose-role';
+
+
+
+// Sign Up Form Questions
+
+export const MENTOR_QUESTIONS = [
+    {
+        name: 'Mentor Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    }
+]
+export const MENTEE_QUESTIONS = [
+    {
+        name: 'Mentee Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    },
+    {
+        name: 'Question 1',
+        placeHolder: 'PlaceHolder',
+        title: 'Title'
+    }
+]
+
+
+
+
