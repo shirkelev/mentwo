76500510212baf4cbb52b2c323f9683391a7f700
yuvalyv <Yuvalyv@gmail.com>
2023-04-29 01:23:03 +0300

    OptinalMentor + ChooseMentorPage

--- src/components/OptionalMentor.js
+++ src/components/OptionalMentor.js
@@ -1,31 +1,34 @@
-import { Avatar, Box } from '@mui/material';
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+import { Stack } from '@mui/material';
+import DetailsModal from './DetailsModal';
 
 
-export default function OptionalMentor() {
-    return ( 
-        <Box
-        sx={{
-        width: 300,
-        height: 300,
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
-      }}>
-        <div>
-        <Avatar
-         alt="Remy Sharp"
-         src="/src/data/images/nitzan.jpg"
-         sx={{ width: 100, height: 100}}
-         
-         ></Avatar>
-         <h1>Nits</h1>
-         <h2>Little Details</h2>
-        </div>
-        
-        </Box>
-
-    );
-  }
+export default function ImgMediaCard({mentorName, details, picturePath}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <Stack 
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      >
+      <Avatar  sx={{ width: 100, height: 100 }} src={picturePath}/>
+      </Stack>
+      
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div"> {mentorName}</Typography>
+        <Typography variant="body2" color="text.secondary">{details}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">About {mentorName}</Button>
+        <Button size="small">Be my Mentor!</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/ChooseMentorPage.js
+++ src/pages/ChooseMentorPage.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import OptionalMentor from '../components/OptionalMentor';
+import { Stack } from '@mui/material';
+import MenteeProcessTimeline from '../components/MenteeProcessTimeline'
 
 const ChooseMentorPage = ({user}) => {
     return(
         <div>
-            <h1>Hi</h1>
-            <OptionalMentor></OptionalMentor>
+        <MenteeProcessTimeline></MenteeProcessTimeline>
+        <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}>
+            
+            <OptionalMentor 
+            mentorName={"Nitsan"}
+            details={"A third year computer science student, work at Mobilye"}
+            picturePath={"src\data\images\nitzan.jpeg"}></OptionalMentor>
+            <OptionalMentor 
+            mentorName={"Yaniv"}
+            details={"A third year computer science student, work at Mobilye"}
+            picturePath={"src\data\images\nitzan.jpeg"}></OptionalMentor>
+            <OptionalMentor 
+            mentorName={"Yehonatan"}
+            details={"A third year computer science student, work at Mobilye"}
+            picturePath={"src\data\images\nitzan.jpeg"}></OptionalMentor>
+        </Stack>
         </div>
       
     );   
