1d0a7049e0e424bf2ac030bac62703eda8850418
nitsanheiman <nitsan.heiman@mail.huji.ac.il>
2023-04-29 18:06:39 +0300

    nitsan: use OptionalMentor component in home page

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -7,6 +7,8 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import DoneIcon from '@mui/icons-material/Done';
 import PersonAddDisabledIcon from '@mui/icons-material/PersonAddDisabled';
+import OptionalMentor from '../components/OptionalMentor';
+import { Stack } from '@mui/material';
 
 const LinesWrapper = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -46,7 +48,18 @@ const HomeContent = ({headline, list}) => {
       </div>
       <h3>{headline}</h3>
     </div>
-    <LinesWrapper>
+    <Stack direction="row" justifyContent="center" alignItems="center" spacing={4}>
+    {list.map(mentee => {
+        return (
+          <>
+          <OptionalMentor picturePath={mentee.img} mentorName={mentee.name} 
+          details={"A third year computer science student, work at Mobileye"} />
+          </>
+        )
+      })}
+    </Stack>
+    
+    {/* <LinesWrapper>
       {list.map(x => {
         return (
           <>
@@ -56,7 +69,7 @@ const HomeContent = ({headline, list}) => {
           </>
         )
       })}
-    </LinesWrapper>
+    </LinesWrapper> */}
     <Divider />
     </>
   );
