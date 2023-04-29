85fd9b743e5f28bae8aea3e01097c96243ffe9bc
nitsanheiman <nitsan.heiman@mail.huji.ac.il>
2023-04-29 14:35:33 +0300

    nitsan: locate capacityBar in homePage

--- src/components/CapacityBar.js
+++ src/components/CapacityBar.js
@@ -2,9 +2,9 @@ import CapacityBattery from "./small-components/CapacityBattery";
 
 const CapacityBar = ({capacity, mentees_num}) => {
     return (
-    <div>
-        <CapacityBattery full_fruction = {mentees_num / capacity}/>
-        <span>Current occupancy: {mentees_num} mentees out of {capacity}.</span>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>
+            <CapacityBattery full_fruction = {mentees_num / capacity}/>
+            <span>{mentees_num}/{capacity} Mentees</span>
     </div>
     )
   };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,19 +6,23 @@ import CapacityBar from '../components/CapacityBar';
 const HomePage = ({user}) => {
   if(user.type === 'mentor'){
     return (
-      <div style={{ width:'100%', height:'100%'}}>
+      <div style={{ width:'100%', height:'100%' }}>
         <NavigationBar />
-        <div style={{padding: "10px", width: '98%'}}>
-          <h1> Hello {user.userName}! </h1>
-          <CapacityBar capacity = {user.capacity} mentees_num = {user.approvedMentees.length} />
-          <h3>Pending Requestes:</h3>
-          <HomeContent list={user.pendingMentees}/>
+        <div style={{ padding: "10px", width: '98%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <h1 style={{ display: 'inline-block' }}>Hello {user.userName}!</h1>
+          <div style={{}}>
+            <CapacityBar capacity={user.capacity} mentees_num={user.approvedMentees.length} />
+          </div>
+        </div>
+        <div style={{ padding: "10px", width: '98%' }}>
+          <h3>Pending Requests:</h3>
+          <HomeContent list={user.pendingMentees} />
           <h3>Active :</h3>
-          <HomeContent list={user.approvedMentees}/>
+          <HomeContent list={user.approvedMentees} />
           <h3>Finished :</h3>
-          <HomeContent list={user.finishedMentees}/>
+          <HomeContent list={user.finishedMentees} />
           <h3>Declined :</h3>
-          <HomeContent list={user.declinedMentees}/>
+          <HomeContent list={user.declinedMentees} />
         </div>
       </div>
     );
