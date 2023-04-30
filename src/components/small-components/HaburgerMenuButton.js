e25623947bc6e030c554ed58eeec30a835254ea4
nitsanheiman <nitsan.heiman@mail.huji.ac.il>
2023-04-30 16:58:36 +0300

    nitsan: open and close menu & click on menu items

--- src/components/small-components/HaburgerMenuButton.js
+++ src/components/small-components/HaburgerMenuButton.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import HamburgerMenu from '../HamburgerMenu';
 
-const HamburgerMenuButton = () => {
+const HamburgerMenuButton = ({ user }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <IconButton>
-      <Menu />
-    </IconButton>
+    <>
+      <IconButton onClick={handleClick}>
+        <MenuIcon />
+      </IconButton>
+      {showMenu && <HamburgerMenu user={user} />}
+    </>
   );
 };
 
