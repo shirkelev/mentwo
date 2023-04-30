e7e4694ec0ed61719f74f25c8a63787f4047be6c
nitsanheiman <nitsan.heiman@mail.huji.ac.il>
2023-04-30 16:59:16 +0300

    nitsan: delete comments

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -47,29 +47,3 @@ export default function HamburgerMenu({user}) {
         );
   }
 }
-
-// const HamburgerMenu = ({ isOpen, handleClose }) => {
-//   return (
-//     <div className={isOpen ? 'open' : 'closed'}>
-//         <div className="hamburger-menu">    
-//             <div className="hamburger-header">
-//                 <div className="logo">
-//                     Your Logo Here
-//                 </div>
-//                 <div className="close-icon" onClick={handleClose}>
-//                     X
-//                 </div>
-//             </div>
-//             <div className="hamburger-body">
-//                 <HamburgerMenuItem text="Item 1" />
-//                 <HamburgerMenuItem text="Item 2" />
-//                 <HamburgerMenuItem text="Item 3" />
-//                 <HamburgerMenuItem text="Item 4" />
-//                 <HamburgerMenuItem text="Item 5" />
-//             </div>
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default HamburgerMenu;
