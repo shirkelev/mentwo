57ce2da20311b2be65e73ab65160b72bfda0a0e0
yuvalyv <Yuvalyv@gmail.com>
2023-06-07 16:55:01 +0300

    fix

--- src/data/DataBase.js
+++ src/data/DataBase.js
@@ -32,9 +32,7 @@ export default class DataBase{
         shir.addMentor(yuvi);
         nitsan.addMentee(yuvi, 'pending');
         nitsan.addMentee(omer, 'finished');
-        shir.addOptionalMentor(yuvi);
-        // shir.addOptionalMentor(omer);
-        // shir.addOptionalMentor(nitsan);
+
         shir.status = 1;
         for(let i = 0; i < 2; i++){
             nitsan.addMentee(new Mentee('Name' + String(i), 'Name' + String(i), 'Last'
