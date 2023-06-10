b0da2fc31eea02f11ae5afc25e4b5f4feb2eaaaf
Shir Levran Kehat <shir.levran.kehat@mail.huji.ac.il>
2023-06-10 20:23:25 +0300

    Shir backend

--- src/components/small-components/RoundButton.js
+++ src/components/small-components/RoundButton.js
@@ -17,28 +17,12 @@ const StyledButton = styled(Button)(({ theme, color }) => ({
   },
 }));
 
-<<<<<<< HEAD
-const RoundButton = ({ text, color, href='', onClick , to='', backgroundImage}) => {
-  return (
-    <div>
-      <StyledButton variant="contained" color={color} href={href} onClick={onClick} style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '130px',
-        height: '130px',
-        }}>
-      </StyledButton>
-      {text}
-      </div>
-=======
 const RoundButton = ({ text, color, href='', onClick , to='', img}) => {
   return (
       <>
       <img src={img} alt="icon" style={{width: '100%', height:'20%'}} onClick={onClick}/>
       <h3 onClick={onClick}> {text} </h3>
       </>
->>>>>>> shir
   );
 };
 
