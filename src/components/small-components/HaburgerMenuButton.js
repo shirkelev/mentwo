2fbd64949c2dc6a875466a87277a9ce5b5de1886
yuvalyv <Yuvalyv@gmail.com>
2023-06-20 13:04:58 +0300

    yuval: top bar

--- src/components/small-components/HaburgerMenuButton.js
+++ src/components/small-components/HaburgerMenuButton.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { HamburgerMenuContext } from '../../context/HamburgerMenuContexts';
+import Avatar from '@mui/material/Avatar';
 
 const HamburgerMenuButton = ({ user }) => {
   const {showMenu, setShowMenu} = useContext(HamburgerMenuContext);
@@ -14,7 +15,7 @@ const HamburgerMenuButton = ({ user }) => {
   return (
     <>
       <IconButton onClick={handleClick}>
-        <MenuIcon />
+        <Avatar alt="Remy Sharp" src={user.img} />
       </IconButton>
       
     </>
