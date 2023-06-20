b90d20a4b1516ddb824147e57307725b89d9e96c
yuvalyv <Yuvalyv@gmail.com>
2023-06-20 18:47:58 +0300

    yuval: loaders

--- src/pages/sign-up/SignUpLoading.js
+++ src/pages/sign-up/SignUpLoading.js
@@ -1,29 +1,27 @@
-import CircularProgressBar from '../../components/small-components/CircularProgressBar'
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import {Stack} from '@mui/material';
+import CircularProgressBar from '../../components/small-components/CircularProgressBar';
 
 
 const SignUpLoading = ({text}) => {
 
-    const RootContainer = styled(Container)(({ theme }) => ({
+    const containerStyles = {
+        height: '80vh',
         display: 'flex',
-        flexDirection: 'column',
-        alignContent: 'center',
         justifyContent: 'center',
-        height: '100%',
-        width: '100%',
-        paddingBottom: theme.spacing(5),
-      }));
+        alignItems: 'center'
+    };
 
     return (
-        <>
-        <RootContainer>
-            <CircularProgressBar />
-            <Typography variant="h5" component="h2" gutterBottom>
-                {text}
-            </Typography>
-        </RootContainer>
-        </>
+       <div style={containerStyles}>
+       <Stack direction="column" justifyContent="center" alignItems="center" spacing={3} maxWidth="80%" marginBottom={20}>
+       <CircularProgressBar></CircularProgressBar>
+       <Typography variant="h5" component="h2" gutterBottom>
+          {text}
+        </Typography>
+       </Stack>
+     </div>
     )};
 export default SignUpLoading;
