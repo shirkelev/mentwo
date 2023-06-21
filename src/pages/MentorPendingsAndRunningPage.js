81add6f2b4389279af9b8b4c2eb6ad7b63df211c
Shir Levran Kehat <shir.levran.kehat@mail.huji.ac.il>
2023-06-21 20:34:57 +0300

    Shir: Availability change screen

--- src/pages/MentorPendingsAndRunningPage.js
+++ src/pages/MentorPendingsAndRunningPage.js
@@ -8,18 +8,18 @@ import { useState } from 'react';
 import IconWithMessage from '../components/small-components/IconwithMessage';
 
 export default function MentorPendingsAndRunningPage ({user}) {
-  let pendingsList = user.pendingMenteesData ? user.pendingMenteesData : [];
+  let pendingsList = user.pendingMenteesData && user.available ? user.pendingMenteesData : [];
   const sliceLength = pendingsList.length > 3 ? 3 : pendingsList.length;
   pendingsList = pendingsList.slice(0, sliceLength)
   console.log("pendingsList", pendingsList,sliceLength);
 
-  const [message, setmessage] = useState(user.pendingMenteesData?.length !== 0 ? '' : "You don't have any pending interviews at the moment");
+  const [message, setmessage] = useState(user.pendingMenteesData?.length !== 0 && user.available ? '' : "You don't have any pending interviews at the moment");
   return (
     <>
      <div style={{ width: '100vw', height: '100vh' }}>
       <div style={{ padding: '10px'}}>
         <HomeContent headline = {Constants.PENDINGS} list={pendingsList} user={user}/>
-        <IconWithMessage message={message} isVisible={user.pendingMenteesData?.length === 0}/>
+        <IconWithMessage message={message} isVisible={user.pendingMenteesData?.length === 0 || !user.available}/>
 
       </div>
       <div style={{ padding: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
