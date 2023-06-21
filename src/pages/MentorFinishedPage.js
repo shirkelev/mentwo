8b94c3f309d719e997d1f1a9df9b935870e0f0e8
Omer Getzler <ogetzler@lightricks.com>
2023-06-21 13:57:36 +0300

    21.6 add iconWithMsg

--- src/pages/MentorFinishedPage.js
+++ src/pages/MentorFinishedPage.js
@@ -5,13 +5,18 @@ import CapacityBar from '../components/CapacityBar';
 import TipBox from '../components/small-components/TipBox';
 import HamburgerMenu from '../components/HamburgerMenu';
 import * as Constants from '../Constants';
+import { useState } from 'react';
+import IconWithMessage from '../components/small-components/IconwithMessage';
 
 export default function MentorFinishedPage ({user}) {
-  
+  const [message, setmessage] = useState(user.finishedMenteesData? '' : "You don't have any completed interviews yet");
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <div style={{ padding: '10px'}}>
         <HomeContent headline = {Constants.FINISHED} list={user.finishedMenteesData ? user.finishedMenteesData : []} user={user}/>
+        <IconWithMessage message={message} isVisible={user.finishedMenteesData}/>
+
       </div>
       <div style={{ padding: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <TipBox tipMessege={"I recommend that at the end of the interview the interviewee will ask for feedback."}/>
