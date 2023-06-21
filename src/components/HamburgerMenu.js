85506264100df0df2e33b703a2658edeadebf764
nitsanheiman <nitsan.heiman@mail.huji.ac.il>
2023-06-21 09:43:02 +0300

    nitsan: fix log out from menu

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -5,16 +5,24 @@ import MenuList from '@mui/material/MenuList';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import InfoIcon from '@mui/icons-material/Info';
+import { UserAuth } from '../context/AuthContext'
 
 import * as Constants from '../Constants';
 import { HamburgerMenuContext } from '../context/HamburgerMenuContexts';
 
 export default function HamburgerMenu({user}) {
   const {showMenu, setShowMenu} = React.useContext(HamburgerMenuContext);
+  const {logOut} = UserAuth();
 
   function handleClick() {
     setShowMenu(false);
   }
+
+  function handelSignOut(){
+    logOut();
+    setShowMenu(false);
+  }
+
       return (
           <Paper sx={{ width: '230px' }}>
               <MenuList dense>
@@ -23,7 +31,7 @@ export default function HamburgerMenu({user}) {
               <HamburgerMenuItem text="About InternView" icon = {InfoIcon}
               path = {"./" + Constants.ABOUT_PAGE} onClick={handleClick}/>
               <HamburgerMenuItem text="Log Out" icon = {LogoutIcon}
-              path = {'../' + Constants.SIGN_IN} onClick={handleClick}/>
+              path = {'../'} onClick={handelSignOut}/>
               </MenuList>
           </Paper>
       );
